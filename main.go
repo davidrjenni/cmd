3c427479dceb90519f66fbb3cdbe2196c067b4e5
David R. Jenni <david.r.jenni@gmail.com>
2015-01-11 11:36:59 +0100

    Initial commit.

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+)
+
+var (
+	addr = flag.String("addr", ":8080", "HTTP listen address")
+	host = flag.String("host", "", "pass all requests to this host")
+)
+
+func main() {
+	flag.Parse()
+	if *host == "" {
+		usage()
+	}
+	u, err := url.Parse(*host)
+	if err != nil {
+		log.Fatal(err)
+	}
+	proxy := httputil.NewSingleHostReverseProxy(u)
+	s := &http.Server{
+		Addr:    *addr,
+		Handler: proxy,
+	}
+	log.Fatal(s.ListenAndServe())
+}
+
+func usage() {
+	fmt.Println("Usage: rproxy -host http[s]://... [-addr ...]")
+	fmt.Println("Flags:")
+	flag.PrintDefaults()
+	os.Exit(2)
+}
